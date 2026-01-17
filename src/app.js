68d12f9354d0bbc0930d7eda7ced3a1fd8a08873
Francisco Valdez <valdez2025@yahoo.com>
2026-01-17 01:42:53 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import { createRouter } from "./utils/router.js";
+import { ProjectsView } from "./views/ProjectsView.js";
+import { ProjectDetailView } from "./views/ProjectDetailView.js";
 
 function HomeView(root){
   root.innerHTML = `
@@ -6,15 +8,6 @@ function HomeView(root){
       <h1>Juniors3cOps Blog</h1>
       <p>Portafolio + blog de ciberseguridad (SOC, Pentesting, Networking).</p>
       <p>Usa el menú para ver <b>Proyectos</b> o <b>Blogs</b>.</p>
-    </div>
-  `;
-}
-
-function ProjectsView(root){
-  root.innerHTML = `
-    <div class="card">
-      <h1>Proyectos</h1>
-      <p>Aquí irán tus tarjetas de proyectos con tecnologías usadas.</p>
     </div>
   `;
 }
@@ -32,7 +25,7 @@ function AdminView(root){
   root.innerHTML = `
     <div class="card">
       <h1>Admin</h1>
-      <p>Luego crearemos el panel conectado a GitHub API.</p>
+      <p>Aquí irá el panel conectado a GitHub API (crear/editar/eliminar).</p>
     </div>
   `;
 }
@@ -40,6 +33,7 @@ function AdminView(root){
 createRouter({
   "/": HomeView,
   "/projects": ProjectsView,
+  "/projects/:id": ProjectDetailView,
   "/blogs": BlogsView,
   "/admin": AdminView,
   "/404": (root) => root.innerHTML = "<div class='card'><h1>404</h1><p>No existe.</p></div>"
